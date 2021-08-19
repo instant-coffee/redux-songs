5dd1ff577468313fd85727abdd99f0412926ec5a
Lachlan <lachlanrm@gmail.com>
2021-08-19 15:46:12 -0700

    Adds logic and display for song list HC

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {ConnectedSongList as SongList } from './SongList'
 
 const App = () => {
-  return <div id="root">App</div>
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList/>
+        </div>
+      </div>
+    </div>
+  )
+
 }
 
 export default App
